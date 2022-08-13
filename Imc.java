b895ee6cf003dfe264acf0272d8d0813d78fbcb5
jonnatanfarias <jonnattanfarias@gmail.com>
2022-08-13 19:32:50 -0300

    meugitjava

--- Imc.java
+++ Imc.java
@@ -1,7 +1,15 @@
+import java.util.PropertyResourceBundle;
+
+import javax.swing.InputMap;
+import javax.swing.JOptionPane;
+
 public class Imc {
 
     public static void main(String[] args) {
+        var peso = Double.parseDouble(JOptionPane.showInputDialog("Olá digite seu peso"));
+        var altura = Double.parseDouble(JOptionPane.showInputDialog("Olá digite sua altura")) / 100;
+        var s = peso / (altura * altura);
 
-        System.out.println("Ola");
+        System.out.println(s);
     }
 }
